a4a34e1d1abda53b3b4c7ce795d0b60e78486e5d
Jennifer Herdzina <jen@Jennifers-MacBook-Pro.local>
2022-04-11 12:15:20 -0500

    initial commit, react-rainbow

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
+import './App.css';
+
+function App() {
+  let [colors, setColors] = useState([
+    'violet', 'blue',
+    'lightblue', 'green',
+    'greenyellow', 'yellow',
+    'orange', 'red'
+  ])
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock key={i} color={color}/>
+    )
+  })
+  return (
+    <div className="App">
+      {colorMap}
+      <ColorForm addColor={addColor}/>
+    </div>
+  );
+}
+
+export default App;
